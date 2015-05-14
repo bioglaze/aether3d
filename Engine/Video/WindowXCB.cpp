1cbfdd338412e7be4ba3bc1f9bb7c1a60248c5a0
Timo Wiren <timo.wiren@gmail.com>
2015-05-14 10:46:01 +0300

    Linux mouse events.

--- Engine/Video/WindowXCB.cpp
+++ Engine/Video/WindowXCB.cpp
@@ -9,8 +9,6 @@
 #include <X11/Xlib-xcb.h>
 #include <GL/glxw.h>
 #include <GL/glx.h>
-//#include <GL/gl.h>
-//#include <cstring> // for strlen
 
 // Based on https://github.com/nxsy/xcb_handmade/blob/master/src/xcb_handmade.cpp
 // Reference to setting up OpenGL in XCB: http://xcb.freedesktop.org/opengl/
@@ -58,30 +56,32 @@ namespace WindowGlobal
         { XK_Right, ae3d::KeyCode::Right },
         { XK_Up, ae3d::KeyCode::Up },
         { XK_Down, ae3d::KeyCode::Down },
-        { XK_Escape, ae3d::KeyCode::Escape }
+        { XK_Escape, ae3d::KeyCode::Escape },
+        { XK_Return, ae3d::KeyCode::Enter },
+        { XK_space, ae3d::KeyCode::Space }
     };
 }
 
 static int setup_and_run( Display* display, xcb_connection_t* connection, int default_screen, xcb_screen_t* screen, int width, int height )
 {
-    int visualID = 0;
     GLXFBConfig* fb_configs = nullptr;
     int num_fb_configs = 0;
     fb_configs = glXGetFBConfigs( display, default_screen, &num_fb_configs );
     
-    if(!fb_configs || num_fb_configs == 0)
+    if (!fb_configs || num_fb_configs == 0)
     {
         std::cerr << "glXGetFBConfigs failed." << std::endl;
         return -1;
     }
     
     /* Select first framebuffer config and query visualID */
+    int visualID = 0;
     GLXFBConfig fb_config = fb_configs[ 0 ];
     glXGetFBConfigAttrib( display, fb_config, GLX_VISUAL_ID, &visualID );
     
     GLXContext context = glXCreateNewContext( display, fb_config, GLX_RGBA_TYPE, 0, True );
     
-    if(!context)
+    if (!context)
     {
         std::cerr << "glXCreateNewContext failed." << std::endl;
         return -1;
@@ -91,7 +91,6 @@ static int setup_and_run( Display* display, xcb_connection_t* connection, int de
     xcb_colormap_t colormap = xcb_generate_id( connection );
     xcb_window_t window = xcb_generate_id( connection );
     
-    /* Create colormap */
     xcb_create_colormap(
                         connection,
                         XCB_COLORMAP_ALLOC_NONE,
@@ -100,10 +99,10 @@ static int setup_and_run( Display* display, xcb_connection_t* connection, int de
                         visualID
                         );
     
-    /* Create window */
-    uint32_t eventmask = XCB_EVENT_MASK_EXPOSURE | XCB_EVENT_MASK_KEY_PRESS;
-    uint32_t valuelist[] = { eventmask, colormap, 0 };
-    uint32_t valuemask = XCB_CW_EVENT_MASK | XCB_CW_COLORMAP;
+    const uint32_t eventmask = XCB_EVENT_MASK_EXPOSURE | XCB_EVENT_MASK_KEY_PRESS | XCB_EVENT_MASK_KEY_RELEASE |
+                               XCB_EVENT_MASK_BUTTON_PRESS | XCB_EVENT_MASK_BUTTON_RELEASE;
+    const uint32_t valuelist[] = { eventmask, colormap, 0 };
+    const uint32_t valuemask = XCB_CW_EVENT_MASK | XCB_CW_COLORMAP;
     
     xcb_create_window(
                       connection,
@@ -119,13 +118,11 @@ static int setup_and_run( Display* display, xcb_connection_t* connection, int de
                       valuelist
                       );
     
-    
-    // NOTE: window must be mapped before glXMakeContextCurrent
     xcb_map_window( connection, window );
     
     GLXWindow glxwindow = glXCreateWindow( display, fb_config, window, 0 );
 
-    if(!window)
+    if (!window)
     {
         xcb_destroy_window( connection, window );
         glXDestroyContext( display, context );
@@ -136,7 +133,7 @@ static int setup_and_run( Display* display, xcb_connection_t* connection, int de
     
     WindowGlobal::drawable = glxwindow;
     
-    if(!glXMakeContextCurrent( display, WindowGlobal::drawable, WindowGlobal::drawable, context ))
+    if (!glXMakeContextCurrent( display, WindowGlobal::drawable, WindowGlobal::drawable, context ))
     {
         xcb_destroy_window( connection, window );
         glXDestroyContext( display, context );
@@ -179,28 +176,22 @@ void ae3d::Window::Create( int width, int height, WindowCreateFlags flags )
         return;
     }
     
-    /* Acquire event queue ownership */
     XSetEventQueueOwner( WindowGlobal::display, XCBOwnsEventQueue );
 
     /* Find XCB screen */
     xcb_screen_t *screen = 0;
-    xcb_screen_iterator_t screen_iter =
-    xcb_setup_roots_iterator(xcb_get_setup(WindowGlobal::connection));
+    xcb_screen_iterator_t screen_iter = xcb_setup_roots_iterator( xcb_get_setup( WindowGlobal::connection ) );
     for (int screen_num = default_screen; screen_iter.rem && screen_num > 0; --screen_num, xcb_screen_next( &screen_iter ));
     screen = screen_iter.data;
     
     WindowGlobal::key_symbols = xcb_key_symbols_alloc( WindowGlobal::connection );
     
-    /* Initialize window and OpenGL context, run main loop and deinitialize */
     /*int retval =*/ setup_and_run( WindowGlobal::display, WindowGlobal::connection, default_screen, screen, width, height );
     
     /* Cleanup */
     //glXDestroyWindow(display, glxwindow);
-    
     //xcb_destroy_window(connection, window);
-    
-    //glXDestroyContext(display, context);
-    
+    //glXDestroyContext(display, context);    
     //XCloseDisplay(display);
     WindowGlobal::isOpen = true;
 }
@@ -214,14 +205,40 @@ void ae3d::Window::PumpEvents()
         //const bool synthetic_event = (event->response_type & 0x80) != 0;
         const uint8_t response_type = event->response_type & ~0x80;
 
-        switch(response_type)
+        switch (response_type)
         {
             case XCB_EVENT_MASK_BUTTON_PRESS:
+            case XCB_EVENT_MASK_BUTTON_RELEASE:
             {
-                xcb_button_press_event_t* be = (xcb_button_press_event_t*)event;
+                const xcb_query_pointer_reply_t* pointer = xcb_query_pointer_reply(WindowGlobal::connection, xcb_query_pointer(WindowGlobal::connection, XDefaultRootWindow(WindowGlobal::display)), NULL);
+                const bool newb1 = (pointer->mask & XCB_BUTTON_MASK_1) != 0;
+                const bool newb2 = (pointer->mask & XCB_BUTTON_MASK_2) != 0;
+                const bool newb3 = (pointer->mask & XCB_BUTTON_MASK_3) != 0;
+                const xcb_button_press_event_t* be = (xcb_button_press_event_t*)event;
+
+                const auto b1Type = response_type == XCB_EVENT_MASK_BUTTON_PRESS ? ae3d::WindowEventType::Mouse1Down : ae3d::WindowEventType::Mouse1Up;
+                const auto b2Type = response_type == XCB_EVENT_MASK_BUTTON_PRESS ? ae3d::WindowEventType::Mouse2Down : ae3d::WindowEventType::Mouse2Up;
+                const auto b3Type = response_type == XCB_EVENT_MASK_BUTTON_PRESS ? ae3d::WindowEventType::MouseMiddleDown : ae3d::WindowEventType::MouseMiddleUp;
 
                 ++WindowGlobal::eventIndex;
-                WindowGlobal::eventStack[ WindowGlobal::eventIndex ].type = ae3d::WindowEventType::Mouse1Down;
+                if (newb1)
+                {
+                    WindowGlobal::eventStack[ WindowGlobal::eventIndex ].type = b1Type;
+                }
+                else if (newb3)
+                {
+                    WindowGlobal::eventStack[ WindowGlobal::eventIndex ].type = b2Type;
+                }
+                else if (newb2)
+                {
+                    WindowGlobal::eventStack[ WindowGlobal::eventIndex ].type = b3Type;
+                }
+                else
+                {
+                    std::cerr << "Unhandled mouse button." << std::endl;
+                    WindowGlobal::eventStack[ WindowGlobal::eventIndex ].type = b1Type;
+                }
+                
                 WindowGlobal::eventStack[ WindowGlobal::eventIndex ].mouseX = be->event_x;
                 WindowGlobal::eventStack[ WindowGlobal::eventIndex ].mouseY = be->event_y;
             }
