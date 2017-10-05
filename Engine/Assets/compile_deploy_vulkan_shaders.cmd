e3ee555d91d2b20f54a7b2f05b07520bcb7b1f7c
Timo Wiren <timo.wiren@gmail.com>
2017-10-05 21:01:11 +0300

    [vulkan] Add batch command for spirv-opt, commented out for now

--- Engine/Assets/compile_deploy_vulkan_shaders.cmd
+++ Engine/Assets/compile_deploy_vulkan_shaders.cmd
@@ -8,4 +8,6 @@ C:\VulkanSDK\1.0.61.0\Bin\glslangValidator -V skybox.frag -o ..\..\..\aether3d_b
 C:\VulkanSDK\1.0.61.0\Bin\glslangValidator -V depthnormals.vert -o ..\..\..\aether3d_build\Samples\depthnormals_vert.spv
 C:\VulkanSDK\1.0.61.0\Bin\glslangValidator -V depthnormals.frag -o ..\..\..\aether3d_build\Samples\depthnormals_frag.spv
 
+REM C:\VulkanSDK\1.0.61.0\Bin\spirv-opt ..\..\..\aether3d_build\Samples\sprite_vert.spv --inline-entry-points-exhaustive --convert-local-access-chains --eliminate-local-single-block --eliminate-local-single-store --eliminate-insert-extract --eliminate-dead-code-aggressive --eliminate-dead-branches --merge-blocks --eliminate-local-single-block --eliminate-local-single-store --eliminate-local-multi-store --eliminate-insert-extract --eliminate-dead-code-aggressive --eliminate-common-uniform -o ..\..\..\aether3d_build\Samples\sprite_vert_opt.spv
+REM C:\VulkanSDK\1.0.61.0\Bin\spirv-remap --strip all --dce all -i ..\..\..\aether3d_build\Samples\sprite_vert_opt.spv -o ..\..\..\aether3d_build\Samples\
 pause
