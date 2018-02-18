689c3b80df97edc6f21190d4fb180669ce483634
Timo Wiren <timo.wiren@gmail.com>
2018-02-18 17:20:15 +0200

    Only 1 copy of nuklear.h

--- Samples/NuklearTest/NuklearTest.cpp
+++ Samples/NuklearTest/NuklearTest.cpp
@@ -232,7 +232,7 @@ int main()
             if (nk_option_label( &ctx, "hard", op == HARD )) op = HARD;
             nk_check_label( &ctx, "check1", 0 );
             nk_check_label( &ctx, "check2", 1 );
-            size_t prog_value = 54;
+            static size_t prog_value = 54;
             nk_progress( &ctx, &prog_value, 100, NK_MODIFIABLE );
 
             static char field_buffer[ 64 ];
@@ -243,7 +243,8 @@ int main()
             nk_property_float( &ctx, "#X:", -1024.0f, &pos, 1024.0f, 1, 1 );
 
             static const char *weapons[] = {"Fist","Pistol","Shotgun" };
-            int current_weapon = nk_combo( &ctx, weapons, 3, current_weapon, 25, nk_vec2( nk_widget_width( &ctx ), 200 ) );
+            static int current_weapon = 0;
+            current_weapon = nk_combo( &ctx, weapons, 3, current_weapon, 25, nk_vec2( nk_widget_width( &ctx ), 200 ) );
 
             nk_layout_row_begin( &ctx, NK_STATIC, 30, 2 );
             {
