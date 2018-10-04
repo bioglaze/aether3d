43709b73269c6b4b7e85313a8835be3faa8b5595
Timo Wiren <timo.wiren@gmail.com>
2018-10-04 19:55:01 +0300

    [vulkan] Add Bloom to shader compile script

--- Engine/Assets/hlsl/Bloom.hlsl
+++ Engine/Assets/hlsl/Bloom.hlsl
@@ -9,21 +9,19 @@
 layout( set = 0, binding = 1 ) Texture2D colorTex : register(t0);
 layout( set = 0, binding = 2 ) RWTexture2D<float4> bloomTex : register(u0);
 
-groupshared uint ldsLightIdx[ 128 ];
-
-float luminance( float3 color )
-{
-    return 0.2126f * color.r + 0.7152f * color.g + 0.0722f * color.b;
-}
+groupshared uint helper[ 128 ];
 
 [numthreads( 8, 8, 1 )]
 void CSMain( uint3 globalIdx : SV_DispatchThreadID, uint3 localIdx : SV_GroupThreadID, uint3 groupIdx : SV_GroupID )
 {
-    float threshold = 0.8f;
-    if (luminance( colorTex.Load( uint3( globalIdx.x, globalIdx.y, 0 ) ).rgb ) > threshold)
+    float4 color = colorTex.Load( uint3( globalIdx.x, globalIdx.y, 0 ) );
+    const float luminance = dot( color.rgb, float3( 0.2126f, 0.7152f, 0.0722f ) );
+    const float threshold = 0.8f;
+    
+    if (luminance > threshold)
     {
         bloomTex[ globalIdx.xy ] = float4(0, 1, 0, 1);
     }
 
-    bloomTex[ globalIdx.xy ] = colorTex.Load( uint3(globalIdx.x, globalIdx.y, 0) );
+    bloomTex[ globalIdx.xy ] = color;
 }
