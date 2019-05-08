30b5a6f2cffc931e663cdc92c2d0fa6deb414f3c
Timo Wiren <timo.wiren@gmail.com>
2019-05-08 19:48:21 +0300

    [vulkan] Line drawing doesn't use index buffer, fixes grid rendering in Editor. [editor] HiDPI awareness on Release VS target

--- Engine/Video/Vulkan/GfxDeviceVulkan.cpp
+++ Engine/Video/Vulkan/GfxDeviceVulkan.cpp
@@ -1936,8 +1936,16 @@ void ae3d::GfxDevice::Draw( VertexBuffer& vertexBuffer, int startIndex, int endI
     VkDeviceSize offsets[ 1 ] = { 0 };
     vkCmdBindVertexBuffers( GfxDeviceGlobal::currentCmdBuffer, VertexBuffer::VERTEX_BUFFER_BIND_ID, 1, vertexBuffer.GetVertexBuffer(), offsets );
 
-    vkCmdBindIndexBuffer( GfxDeviceGlobal::currentCmdBuffer, *vertexBuffer.GetIndexBuffer(), 0, VK_INDEX_TYPE_UINT16 );
-    vkCmdDrawIndexed( GfxDeviceGlobal::currentCmdBuffer, (endIndex - startIndex) * 3, 1, startIndex * 3, 0, 0 );
+    if (topology == PrimitiveTopology::Triangles)
+    {
+        vkCmdBindIndexBuffer( GfxDeviceGlobal::currentCmdBuffer, *vertexBuffer.GetIndexBuffer(), 0, VK_INDEX_TYPE_UINT16 );
+        vkCmdDrawIndexed( GfxDeviceGlobal::currentCmdBuffer, (endIndex - startIndex) * 3, 1, startIndex * 3, 0, 0 );
+    }
+    else if (topology == PrimitiveTopology::Lines)
+    {
+        vkCmdDraw( GfxDeviceGlobal::currentCmdBuffer, (endIndex - startIndex) * 3, 1, startIndex * 3, 0 );
+    }
+
     Statistics::IncTriangleCount( endIndex - startIndex );
     Statistics::IncDrawCalls();
 }
