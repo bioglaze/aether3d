74419e9cf085ffe5455a0a018a1cf405efd4bab0
Timo Wiren <timo.wiren@gmail.com>
2020-05-25 16:09:17 +0300

    [vulkan] Fix a validation warning

--- Engine/Video/Vulkan/GfxDeviceVulkan.cpp
+++ Engine/Video/Vulkan/GfxDeviceVulkan.cpp
@@ -503,7 +503,7 @@ namespace ae3d
 
         vkCmdPipelineBarrier(
             GfxDeviceGlobal::prePresentCmdBuffer,
-            VK_PIPELINE_STAGE_ALL_COMMANDS_BIT,
+            VK_PIPELINE_STAGE_ALL_COMMANDS_BIT, 
             VK_PIPELINE_STAGE_TOP_OF_PIPE_BIT,
             0,
             0, nullptr,
