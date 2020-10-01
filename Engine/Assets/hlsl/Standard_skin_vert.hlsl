6e803b1f608818e0b6b81572a9c9e55387fab8e0
Timo Wiren <timo.wiren@gmail.com>
2020-10-01 16:54:37 +0300

    [hlsl] Skin also normals and tangents

--- Engine/Assets/hlsl/Standard_skin_vert.hlsl
+++ Engine/Assets/hlsl/Standard_skin_vert.hlsl
@@ -35,15 +35,17 @@ PS_INPUT main( VS_INPUT input )
                            boneMatrices[ input.boneIndex.z ] * input.boneWeights.z +
                            boneMatrices[ input.boneIndex.w ] * input.boneWeights.w;
     const float4 position = mul( boneTransform, float4( input.pos, 1.0f ) );
+    const float4 normal = mul( boneTransform, float4( input.normal, 0.0f ) );
+    const float4 tangent = mul( boneTransform, float4( input.tangent.xyz, 0.0f ) );
 
     PS_INPUT output = (PS_INPUT)0;
 
     output.pos = mul( localToClip, position );
     output.positionVS_u = float4( mul( localToView, position ).xyz, input.uv.x );
     output.positionWS_v = float4( mul( localToWorld, position ).xyz, input.uv.y );
-    output.normalVS = mul( localToView, float4(input.normal, 0) ).xyz;
-    output.tangentVS = mul( localToView, float4(input.tangent.xyz, 0) ).xyz;
-    float3 ct = cross( input.normal, input.tangent.xyz ) * input.tangent.w;
+    output.normalVS = mul( localToView, normal ).xyz;
+    output.tangentVS = mul( localToView, float4( tangent.xyz, 0 ) ).xyz;
+    float3 ct = cross( normal.xyz, tangent.xyz ) * input.tangent.w;
     output.bitangentVS.xyz = mul( localToView, float4( ct, 0 ) ).xyz;
 
     return output;
--- Engine/Assets/hlsl/depthnormals_skin_vert.hlsl
+++ Engine/Assets/hlsl/depthnormals_skin_vert.hlsl
@@ -16,13 +16,15 @@ struct VSOutput
 VSOutput main( float3 pos : POSITION, float2 uv : TEXCOORD, float3 normal : NORMAL, float4 tangent : TANGENT, float4 color : COLOR, float4 boneWeights : WEIGHTS, uint4 boneIndex : BONES )
 {
     VSOutput vsOut;
-    float4 position2 = mul( boneMatrices[ boneIndex.x ], float4( pos, 1.0f ) ) * boneWeights.x;
-    position2 += mul( boneMatrices[ boneIndex.y ], float4( pos, 1.0f ) ) * boneWeights.y;
-    position2 += mul( boneMatrices[ boneIndex.z ], float4( pos, 1.0f ) ) * boneWeights.z;
-    position2 += mul( boneMatrices[ boneIndex.w ], float4( pos, 1.0f ) ) * boneWeights.w;
+    matrix boneTransform = boneMatrices[ boneIndex.x ] * boneWeights.x + 
+                           boneMatrices[ boneIndex.y ] * boneWeights.y +
+                           boneMatrices[ boneIndex.z ] * boneWeights.z +
+                           boneMatrices[ boneIndex.w ] * boneWeights.w;
+    const float4 position2 = mul( boneTransform, float4( pos, 1.0f ) );
+    const float4 normal2 = mul( boneTransform, float4( normal, 0.0f ) );
 
     vsOut.pos = mul( localToClip, position2 );
-    vsOut.mvPosition = mul( localToView, float4( pos, 1.0 ) ).xyz;
-    vsOut.normal = mul( localToView, float4( normal, 0.0 ) ).xyz;
+    vsOut.mvPosition = mul( localToView, position2 ).xyz;
+    vsOut.normal = mul( localToView, normal2 ).xyz;
     return vsOut;
 }
