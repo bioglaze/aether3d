30f4c8a09296653fc33a83f0877809df813fffeb
Timo Wiren <timo.wiren@gmail.com>
2020-11-18 20:31:51 +0200

    [editor] SSAO Metal fix

--- Tools/Editor/Editor/SceneView.cpp
+++ Tools/Editor/Editor/SceneView.cpp
@@ -382,7 +382,7 @@ void svInit( SceneView** sv, int width, int height )
     (*sv)->bloomTex.CreateUAV( width / 2, height / 2, "bloomTex", DataType::UByte );
     (*sv)->blurTex.CreateUAV( width / 2, height / 2, "blurTex", DataType::UByte );
     (*sv)->blurTex2.CreateUAV( width / 2, height / 2, "blur2Tex", DataType::UByte );
-    (*sv)->ssaoTex.CreateUAV( width, height, "ssaoTex", DataType::UByte );
+    (*sv)->ssaoTex.CreateUAV( width, height, "ssaoTex", DataType::Float );
 
     constexpr int noiseDim = 64;
     Vec4 noiseData[ noiseDim * noiseDim ];
@@ -540,17 +540,23 @@ void svBeginRender( SceneView* sv )
         int width = sv->ssaoTex.GetWidth();
         int height = sv->ssaoTex.GetHeight();
 
-        sv->ssaoTex.SetLayout( TextureLayout::General );
+        sv->ssaoShader.SetProjectionMatrix( sv->camera.GetComponent<CameraComponent>()->GetProjection() );
         sv->ssaoShader.SetRenderTexture( 0, &sv->cameraTarget );
         sv->ssaoShader.SetRenderTexture( 1, &sv->camera.GetComponent<CameraComponent>()->GetDepthNormalsTexture() );
+#if RENDERER_VULKAN
+        sv->ssaoTex.SetLayout( TextureLayout::General );
         sv->ssaoShader.SetTexture2D( 2, &sv->noiseTex );
         sv->ssaoShader.SetTexture2D( 14, &sv->ssaoTex );
-        sv->ssaoShader.SetProjectionMatrix( sv->camera.GetComponent<CameraComponent>()->GetProjection() );
         sv->ssaoShader.Begin();
+#else
+        sv->ssaoShader.SetTexture2D( 3, &sv->noiseTex );
+        sv->ssaoShader.SetTexture2D( 2, &sv->ssaoTex );
+#endif
         sv->ssaoShader.Dispatch( width / 8, height / 8, 1, "SSAO" );
+#if RENDERER_VULKAN
         sv->ssaoShader.End();
         sv->ssaoTex.SetLayout( TextureLayout::ShaderRead );
-
+#endif
         System::Draw( &sv->ssaoTex, 0, 0, width, height, width, height, Vec4( 1, 1, 1, 1 ), System::BlendMode::Off );
     }
 }
