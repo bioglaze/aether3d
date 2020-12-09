0dc3547888a6eb52b7f9dbbaccfb50b1e282d83b
Timo Wiren <timo.wiren@gmail.com>
2020-12-09 18:41:16 +0200

    Add LineRendererComponent

---
+++ Engine/Components/LineRendererComponent.cpp
@@ -0,0 +1,26 @@
+#include "LineRendererComponent.hpp"
+
+ae3d::LineRendererComponent::LineRendererComponent()
+{
+
+}
+
+static constexpr int MaxComponents = 30;
+
+ae3d::LineRendererComponent lineRendererComponents[ MaxComponents ];
+unsigned nextFreeLineRendererComponent = 0;
+
+unsigned ae3d::LineRendererComponent::New()
+{
+    if (nextFreeLineRendererComponent == MaxComponents - 1)
+    {
+        return nextFreeLineRendererComponent;
+    }
+    
+    return nextFreeLineRendererComponent++;
+}
+
+ae3d::LineRendererComponent* ae3d::LineRendererComponent::Get( unsigned index )
+{
+    return &lineRendererComponents[ index ];
+}
---
+++ Engine/Include/LineRendererComponent.hpp
@@ -0,0 +1,47 @@
+#pragma once
+
+#include <string>
+
+namespace ae3d
+{
+    /// Renders lines. Use this component to get proper depth testing relative to other objects in a scene.
+    /// For debug visualization lines you can use System::DrawLines() which is simpler.
+    class LineRendererComponent
+    {
+    public:
+        /// Constructor.
+        LineRendererComponent();
+
+        /// \return GameObject that owns this component.
+        class GameObject* GetGameObject() const { return gameObject; }
+
+        /// \param enabled True if the component should be rendered, false otherwise.
+        void SetEnabled( bool enabled ) { isEnabled = enabled; }
+
+        /// \return Line handle.
+        int GetLineHandle( int handle ) const { return lineHandle; }
+
+        /// \param handle Line handle. Use System::CreateLineBuffer() to generate the handle.
+        void SetLineHandle( int handle ) { lineHandle = handle; }
+
+    private:
+        friend class GameObject;
+        friend class Scene;
+        
+        /* \return Component's type code. Must be unique for each component type. */
+        static int Type() { return 10; }
+        
+        /* \return Component handle that uniquely identifies the instance. */
+        static unsigned New();
+        
+        /* \return Component at index or null if index is invalid. */
+        static LineRendererComponent* Get( unsigned index );
+
+        /* \param localToClip Transforms coordinates to clip space. */
+        void Render( const float* localToClip );
+                
+        GameObject* gameObject = nullptr;
+        int lineHandle = 0;
+        bool isEnabled = true;
+    };
+}
