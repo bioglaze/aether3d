d2beb801309669034b9650b16007e5e31aca1a41
Timo Wiren <timo.wiren@gmail.com>
2021-03-02 19:19:45 +0200

    [vulkan] Compile shaders using DXC

--- Engine/Assets/compile_deploy_vulkan_shaders.cmd
+++ Engine/Assets/compile_deploy_vulkan_shaders.cmd
@@ -1,28 +1,28 @@
-%VULKAN_SDK%\bin\glslangvalidator.exe -D -V -S vert -e main hlsl\sprite_vert.hlsl -o ..\..\..\aether3d_build\Samples\shaders\sprite_vert.spv
-%VULKAN_SDK%\bin\glslangvalidator.exe -D -V -S frag -e main hlsl\sprite_frag.hlsl -o ..\..\..\aether3d_build\Samples\shaders\sprite_frag.spv
-%VULKAN_SDK%\bin\glslangvalidator.exe -D -V -S vert -e main hlsl\unlit_cube_vert.hlsl -o ..\..\..\aether3d_build\Samples\shaders\unlit_cube_vert.spv
-%VULKAN_SDK%\bin\glslangvalidator.exe -D -V -S frag -e main hlsl\unlit_cube_frag.hlsl -o ..\..\..\aether3d_build\Samples\shaders\unlit_cube_frag.spv
-%VULKAN_SDK%\bin\glslangvalidator.exe -D -V -S vert -e main hlsl\unlit_vert.hlsl -o ..\..\..\aether3d_build\Samples\shaders\unlit_vert.spv
-%VULKAN_SDK%\bin\glslangvalidator.exe -D -V -S frag -e main hlsl\unlit_frag.hlsl -o ..\..\..\aether3d_build\Samples\shaders\unlit_frag.spv
-%VULKAN_SDK%\bin\glslangvalidator.exe -D -V -S vert -e main hlsl\unlit_skin_vert.hlsl -o ..\..\..\aether3d_build\Samples\shaders\unlit_skin_vert.spv
-%VULKAN_SDK%\bin\glslangvalidator.exe -D -V -S vert -e main hlsl\moments_vert.hlsl -o ..\..\..\aether3d_build\Samples\shaders\moments_vert.spv
-%VULKAN_SDK%\bin\glslangvalidator.exe -D -V -S vert -e main hlsl\moments_skin_vert.hlsl -o ..\..\..\aether3d_build\Samples\shaders\moments_skin_vert.spv
-%VULKAN_SDK%\bin\glslangvalidator.exe -D -V -S frag -e main hlsl\moments_frag.hlsl -o ..\..\..\aether3d_build\Samples\shaders\moments_frag.spv
-%VULKAN_SDK%\bin\glslangvalidator.exe -D -V -S vert -e main hlsl\skybox_vert.hlsl -o ..\..\..\aether3d_build\Samples\shaders\skybox_vert.spv
-%VULKAN_SDK%\bin\glslangvalidator.exe -D -V -S frag -e main hlsl\skybox_frag.hlsl -o ..\..\..\aether3d_build\Samples\shaders\skybox_frag.spv
-%VULKAN_SDK%\bin\glslangvalidator.exe -D -V -S vert -e main hlsl\depthnormals_vert.hlsl -o ..\..\..\aether3d_build\Samples\shaders\depthnormals_vert.spv
-%VULKAN_SDK%\bin\glslangvalidator.exe -D -V -S vert -e main hlsl\depthnormals_skin_vert.hlsl -o ..\..\..\aether3d_build\Samples\shaders\depthnormals_skin_vert.spv
-%VULKAN_SDK%\bin\glslangvalidator.exe -D -V -S frag -e main hlsl\depthnormals_frag.hlsl -o ..\..\..\aether3d_build\Samples\shaders\depthnormals_frag.spv
-%VULKAN_SDK%\bin\glslangvalidator.exe -D -V -S comp -e CSMain hlsl\LightCuller.hlsl -o ..\..\..\aether3d_build\Samples\shaders\LightCuller.spv
-%VULKAN_SDK%\bin\glslangvalidator.exe -D -V -S vert -e main hlsl\Standard_vert.hlsl -o ..\..\..\aether3d_build\Samples\shaders\Standard_vert.spv
-%VULKAN_SDK%\bin\glslangvalidator.exe -D -V -S vert -e main hlsl\Standard_skin_vert.hlsl -o ..\..\..\aether3d_build\Samples\shaders\Standard_skin_vert.spv
-%VULKAN_SDK%\bin\glslangvalidator.exe -D -V -S frag -e main hlsl\Standard_frag.hlsl -o ..\..\..\aether3d_build\Samples\shaders\Standard_frag.spv
-%VULKAN_SDK%\bin\glslangvalidator.exe -D -DENABLE_SHADOWS -V -S frag -e main hlsl\Standard_frag.hlsl -o ..\..\..\aether3d_build\Samples\shaders\Standard_frag_shadow.spv
-%VULKAN_SDK%\bin\glslangvalidator.exe -D -DENABLE_SHADOWS_POINT -V -S frag -e main hlsl\Standard_frag.hlsl -o ..\..\..\aether3d_build\Samples\shaders\Standard_frag_shadow_point.spv
-%VULKAN_SDK%\bin\glslangvalidator.exe -D -V -S comp -e CSMain hlsl\Bloom.hlsl -o ..\..\..\aether3d_build\Samples\shaders\Bloom.spv
-%VULKAN_SDK%\bin\glslangvalidator.exe -D -V -S comp -e CSMain hlsl\Blur.hlsl -o ..\..\..\aether3d_build\Samples\shaders\Blur.spv
-%VULKAN_SDK%\bin\glslangvalidator.exe -D -V -S comp -e CSMain hlsl\ssao.hlsl -o ..\..\..\aether3d_build\Samples\shaders\ssao.spv
-%VULKAN_SDK%\bin\glslangvalidator.exe -D -V -S comp -e CSMain hlsl\compose.hlsl -o ..\..\..\aether3d_build\Samples\shaders\compose.spv
+%VULKAN_SDK%\bin\dxc.exe -DVULKAN -Ges -spirv -E main -all-resources-bound -T vs_6_0 hlsl\sprite_vert.hlsl -Fo ..\..\..\aether3d_build\Samples\shaders\sprite_vert.spv
+%VULKAN_SDK%\bin\dxc.exe -DVULKAN -Ges -spirv -E main -all-resources-bound -T ps_6_0 hlsl\sprite_frag.hlsl -Fo ..\..\..\aether3d_build\Samples\shaders\sprite_frag.spv
+%VULKAN_SDK%\bin\dxc.exe -DVULKAN -Ges -spirv -E main -all-resources-bound -T vs_6_0 hlsl\unlit_cube_vert.hlsl -Fo ..\..\..\aether3d_build\Samples\shaders\unlit_cube_vert.spv
+%VULKAN_SDK%\bin\dxc.exe -DVULKAN -Ges -spirv -E main -all-resources-bound -T ps_6_0 hlsl\unlit_cube_frag.hlsl -Fo ..\..\..\aether3d_build\Samples\shaders\unlit_cube_frag.spv
+%VULKAN_SDK%\bin\dxc.exe -DVULKAN -Ges -spirv -E main -all-resources-bound -T vs_6_0 hlsl\unlit_vert.hlsl -Fo ..\..\..\aether3d_build\Samples\shaders\unlit_vert.spv
+%VULKAN_SDK%\bin\dxc.exe -DVULKAN -Ges -spirv -E main -all-resources-bound -T ps_6_0 hlsl\unlit_frag.hlsl -Fo ..\..\..\aether3d_build\Samples\shaders\unlit_frag.spv
+%VULKAN_SDK%\bin\dxc.exe -DVULKAN -Ges -spirv -E main -all-resources-bound -T vs_6_0 hlsl\unlit_skin_vert.hlsl -Fo ..\..\..\aether3d_build\Samples\shaders\unlit_skin_vert.spv
+%VULKAN_SDK%\bin\dxc.exe -DVULKAN -Ges -spirv -E main -all-resources-bound -T vs_6_0 hlsl\moments_vert.hlsl -Fo ..\..\..\aether3d_build\Samples\shaders\moments_vert.spv
+%VULKAN_SDK%\bin\dxc.exe -DVULKAN -Ges -spirv -E main -all-resources-bound -T vs_6_0 hlsl\moments_skin_vert.hlsl -Fo ..\..\..\aether3d_build\Samples\shaders\moments_skin_vert.spv
+%VULKAN_SDK%\bin\dxc.exe -DVULKAN -Ges -spirv -E main -all-resources-bound -T ps_6_0 hlsl\moments_frag.hlsl -Fo ..\..\..\aether3d_build\Samples\shaders\moments_frag.spv
+%VULKAN_SDK%\bin\dxc.exe -DVULKAN -Ges -spirv -E main -all-resources-bound -T vs_6_0 hlsl\skybox_vert.hlsl -Fo ..\..\..\aether3d_build\Samples\shaders\skybox_vert.spv
+%VULKAN_SDK%\bin\dxc.exe -DVULKAN -Ges -spirv -E main -all-resources-bound -T ps_6_0 hlsl\skybox_frag.hlsl -Fo ..\..\..\aether3d_build\Samples\shaders\skybox_frag.spv
+%VULKAN_SDK%\bin\dxc.exe -DVULKAN -Ges -spirv -E main -all-resources-bound -T vs_6_0 hlsl\depthnormals_vert.hlsl -Fo ..\..\..\aether3d_build\Samples\shaders\depthnormals_vert.spv
+%VULKAN_SDK%\bin\dxc.exe -DVULKAN -Ges -spirv -E main -all-resources-bound -T vs_6_0 hlsl\depthnormals_skin_vert.hlsl -Fo ..\..\..\aether3d_build\Samples\shaders\depthnormals_skin_vert.spv
+%VULKAN_SDK%\bin\dxc.exe -DVULKAN -Ges -spirv -E main -all-resources-bound -T ps_6_0 hlsl\depthnormals_frag.hlsl -Fo ..\..\..\aether3d_build\Samples\shaders\depthnormals_frag.spv
+%VULKAN_SDK%\bin\dxc.exe -DVULKAN -Ges -spirv -E CSMain -all-resources-bound -T cs_6_0 hlsl\LightCuller.hlsl -Fo ..\..\..\aether3d_build\Samples\shaders\LightCuller.spv
+%VULKAN_SDK%\bin\dxc.exe -DVULKAN -Ges -spirv -E main -all-resources-bound -T vs_6_0 hlsl\Standard_vert.hlsl -Fo ..\..\..\aether3d_build\Samples\shaders\Standard_vert.spv
+%VULKAN_SDK%\bin\dxc.exe -DVULKAN -Ges -spirv -E main -all-resources-bound -T vs_6_0 hlsl\Standard_skin_vert.hlsl -Fo ..\..\..\aether3d_build\Samples\shaders\Standard_skin_vert.spv
+%VULKAN_SDK%\bin\dxc.exe -DVULKAN -Ges -spirv -E main -all-resources-bound -T ps_6_0 hlsl\Standard_frag.hlsl -Fo ..\..\..\aether3d_build\Samples\shaders\Standard_frag.spv
+%VULKAN_SDK%\bin\dxc.exe -DVULKAN -DENABLE_SHADOWS -Ges -spirv -E main -all-resources-bound -T ps_6_0 hlsl\Standard_frag.hlsl -Fo ..\..\..\aether3d_build\Samples\shaders\Standard_frag_shadow.spv
+%VULKAN_SDK%\bin\dxc.exe -DVULKAN -DENABLE_SHADOWS_POINT -Ges -spirv -E main -all-resources-bound -T ps_6_0 hlsl\Standard_frag.hlsl -Fo ..\..\..\aether3d_build\Samples\shaders\Standard_frag_shadow_point.spv
+%VULKAN_SDK%\bin\dxc.exe -DVULKAN -Ges -spirv -E CSMain -all-resources-bound -T cs_6_0 hlsl\Bloom.hlsl -Fo ..\..\..\aether3d_build\Samples\shaders\Bloom.spv
+%VULKAN_SDK%\bin\dxc.exe -DVULKAN -Ges -spirv -E CSMain -all-resources-bound -T cs_6_0 hlsl\Blur.hlsl -Fo ..\..\..\aether3d_build\Samples\shaders\Blur.spv
+%VULKAN_SDK%\bin\dxc.exe -DVULKAN -Ges -spirv -E CSMain -all-resources-bound -T cs_6_0 hlsl\ssao.hlsl -Fo ..\..\..\aether3d_build\Samples\shaders\ssao.spv
+%VULKAN_SDK%\bin\dxc.exe -DVULKAN -Ges -spirv -E CSMain -all-resources-bound -T cs_6_0 hlsl\compose.hlsl -Fo ..\..\..\aether3d_build\Samples\shaders\compose.spv
 
 @REM %VULKAN_SDK%\Bin\spirv-opt hlsl\sprite_vert.spv --inline-entry-points-exhaustive --convert-local-access-chains --eliminate-local-single-block --eliminate-local-single-store --eliminate-insert-extract --eliminate-dead-code-aggressive --eliminate-dead-branches --merge-blocks --eliminate-local-single-block --eliminate-local-single-store --eliminate-local-multi-store --eliminate-insert-extract --eliminate-dead-code-aggressive --eliminate-common-uniform -o ..\..\..\aether3d_build\Samples\sprite_vert_opt.spv
 @REM %VULKAN_SDK%\Bin\spirv-remap --strip all --dce all -i hlsl\sprite_vert_opt.spv -o ..\..\..\aether3d_build\Samples\
--- Engine/Assets/hlsl/ubo.h
+++ Engine/Assets/hlsl/ubo.h
@@ -1,17 +1,12 @@
 #if !VULKAN
-#define layout(a,b)  
-#else
-#define register(a) blank
-#endif
-
-layout( set = 0, binding = 0 ) Texture2D tex : register(t0);
-layout( set = 0, binding = 1 ) Texture2D normalTex : register(t1);
-layout( set = 0, binding = 2 ) Texture2D specularTex : register(t2);
-layout( set = 0, binding = 3 ) Texture2D shadowTex : register(t3);
-layout( set = 0, binding = 4 ) TextureCube<float4> texCube : register(t4);
-layout( set = 0, binding = 5 ) SamplerState sLinear : register(s0);
-layout( set = 0, binding = 6 ) SamplerState sampler1 : register(s1);
-layout( set = 0, binding = 7 ) cbuffer cbPerFrame : register(b0)
+Texture2D tex : register(t0);
+Texture2D normalTex : register(t1);
+Texture2D specularTex : register(t2);
+Texture2D shadowTex : register(t3);
+TextureCube<float4> texCube : register(t4);
+SamplerState sLinear : register(s0);
+SamplerState sampler1 : register(s1);
+cbuffer cbPerFrame : register(b0)
 {
     matrix localToClip;
     matrix localToView;
@@ -39,10 +34,56 @@ layout( set = 0, binding = 7 ) cbuffer cbPerFrame : register(b0)
     float4 kernelOffsets[ 16 ];
     float4 cameraParams; // .x: fov (radians), .y: aspect, .z: near, .w: far
 };
-layout( set = 0, binding = 8 ) Buffer<float4> pointLightBufferCenterAndRadius : register(t5);
-layout( set = 0, binding = 9 ) RWBuffer<uint> perTileLightIndexBuffer : register(u0);
-layout( set = 0, binding = 10 ) Buffer<float4> pointLightColors : register(t6);
-layout( set = 0, binding = 11 ) Buffer<float4> spotLightBufferCenterAndRadius : register(t7);
-layout( set = 0, binding = 12 ) Buffer<float4> spotLightParams : register(t8);
-layout( set = 0, binding = 13 ) Buffer<float4> spotLightColors : register(t9);
-layout( set = 0, binding = 14 ) RWTexture2D<float4> rwTexture : register(u1);
+Buffer<float4> pointLightBufferCenterAndRadius : register(t5);
+RWBuffer<uint> perTileLightIndexBuffer : register(u0);
+Buffer<float4> pointLightColors : register(t6);
+Buffer<float4> spotLightBufferCenterAndRadius : register(t7);
+Buffer<float4> spotLightParams : register(t8);
+Buffer<float4> spotLightColors : register(t9);
+RWTexture2D<float4> rwTexture : register(u1);
+
+#else
+
+[[vk::binding( 0 )]] Texture2D tex;
+[[vk::binding( 1 )]] Texture2D normalTex;
+[[vk::binding( 2 )]] Texture2D specularTex;
+[[vk::binding( 3 )]] Texture2D shadowTex;
+[[vk::binding( 4 )]] TextureCube<float4> texCube;
+[[vk::binding( 5 )]] SamplerState sLinear;
+[[vk::binding( 6 )]] SamplerState sampler1;
+[[vk::binding( 7 )]] cbuffer cbPerFrame
+{
+    matrix localToClip;
+    matrix localToView;
+    matrix localToWorld;
+    matrix localToShadowClip;
+    matrix clipToView;
+    matrix viewToClip;
+    float4 lightPosition;
+    float4 lightDirection;
+    float4 lightColor;
+    float lightConeAngle;
+    int lightType;
+    float minAmbient;
+    uint maxNumLightsPerTile;
+    uint windowWidth;
+    uint windowHeight;
+    uint numLights; // 16 bits for point light count, 16 for spot light count
+    float f0;
+    float4 tex0scaleOffset;
+    float4 tilesXY;
+    matrix boneMatrices[ 80 ];
+    int isVR;
+    int kernelSize;
+    float2 bloomParams;
+    float4 kernelOffsets[ 16 ];
+    float4 cameraParams; // .x: fov (radians), .y: aspect, .z: near, .w: far
+};
+[[vk::binding( 8 )]] Buffer<float4> pointLightBufferCenterAndRadius;
+[[vk::binding( 9 )]] RWBuffer<uint> perTileLightIndexBuffer;
+[[vk::binding( 10 )]] Buffer<float4> pointLightColors;
+[[vk::binding( 11 )]] Buffer<float4> spotLightBufferCenterAndRadius;
+[[vk::binding( 12 )]] Buffer<float4> spotLightParams;
+[[vk::binding( 13 )]] Buffer<float4> spotLightColors;
+[[vk::binding( 14 )]] RWTexture2D<float4> rwTexture;
+#endif
