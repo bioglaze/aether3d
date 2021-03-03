816a2c5f216212e5aa5f759e1c50ad8702cf9d80
Timo Wiren <timo.wiren@gmail.com>
2021-03-03 16:04:04 +0200

    [vulkan] Fix shader compile script

--- Engine/Assets/compile_shaders.sh
+++ Engine/Assets/compile_shaders.sh
@@ -1,28 +1,26 @@
-glslangValidator -D -V -S vert -e main hlsl/sprite_vert.hlsl -o ../../../aether3d_build/Samples/shaders/sprite_vert.spv
-
-dxc -DVULKAN -Ges -spirv -E main -all-resources-bound -T vs_6_0 hlsl\sprite_vert.hlsl -Fo ../../../aether3d_build\Samples\shaders\sprite_vert.spv
-dxc -DVULKAN -Ges -spirv -E main -all-resources-bound -T ps_6_0 hlsl\sprite_frag.hlsl -Fo ../../../aether3d_build\Samples\shaders\sprite_frag.spv
-dxc -DVULKAN -Ges -spirv -E main -all-resources-bound -T vs_6_0 hlsl\unlit_cube_vert.hlsl -Fo ../../../aether3d_build\Samples\shaders\unlit_cube_vert.spv
-dxc -DVULKAN -Ges -spirv -E main -all-resources-bound -T ps_6_0 hlsl\unlit_cube_frag.hlsl -Fo ../../../aether3d_build\Samples\shaders\unlit_cube_frag.spv
-dxc -DVULKAN -Ges -spirv -E main -all-resources-bound -T vs_6_0 hlsl\unlit_vert.hlsl -Fo ../../../aether3d_build\Samples\shaders\unlit_vert.spv
-dxc -DVULKAN -Ges -spirv -E main -all-resources-bound -T ps_6_0 hlsl\unlit_frag.hlsl -Fo ../../../aether3d_build\Samples\shaders\unlit_frag.spv
-dxc -DVULKAN -Ges -spirv -E main -all-resources-bound -T vs_6_0 hlsl\unlit_skin_vert.hlsl -Fo ../../../aether3d_build\Samples\shaders\unlit_skin_vert.spv
-dxc -DVULKAN -Ges -spirv -E main -all-resources-bound -T vs_6_0 hlsl\moments_vert.hlsl -Fo ../../../aether3d_build\Samples\shaders\moments_vert.spv
-dxc -DVULKAN -Ges -spirv -E main -all-resources-bound -T vs_6_0 hlsl\moments_skin_vert.hlsl -Fo ../../../aether3d_build\Samples\shaders\moments_skin_vert.spv
-dxc -DVULKAN -Ges -spirv -E main -all-resources-bound -T ps_6_0 hlsl\moments_frag.hlsl -Fo ../../../aether3d_build\Samples\shaders\moments_frag.spv
-dxc -DVULKAN -Ges -spirv -E main -all-resources-bound -T vs_6_0 hlsl\skybox_vert.hlsl -Fo ../../../aether3d_build\Samples\shaders\skybox_vert.spv
-dxc -DVULKAN -Ges -spirv -E main -all-resources-bound -T ps_6_0 hlsl\skybox_frag.hlsl -Fo ../../../aether3d_build\Samples\shaders\skybox_frag.spv
-dxc -DVULKAN -Ges -spirv -E main -all-resources-bound -T vs_6_0 hlsl\depthnormals_vert.hlsl -Fo ../../../aether3d_build\Samples\shaders\depthnormals_vert.spv
-dxc -DVULKAN -Ges -spirv -E main -all-resources-bound -T vs_6_0 hlsl\depthnormals_skin_vert.hlsl -Fo ../../../aether3d_build\Samples\shaders\depthnormals_skin_vert.spv
-dxc -DVULKAN -Ges -spirv -E main -all-resources-bound -T ps_6_0 hlsl\depthnormals_frag.hlsl -Fo ../../../aether3d_build\Samples\shaders\depthnormals_frag.spv
-dxc -DVULKAN -Ges -spirv -E CSMain -all-resources-bound -T cs_6_0 hlsl\LightCuller.hlsl -Fo ../../../aether3d_build\Samples\shaders\LightCuller.spv
-dxc -DVULKAN -Ges -spirv -E main -all-resources-bound -T vs_6_0 hlsl\Standard_vert.hlsl -Fo ../../../aether3d_build\Samples\shaders\Standard_vert.spv
-dxc -DVULKAN -Ges -spirv -E main -all-resources-bound -T vs_6_0 hlsl\Standard_skin_vert.hlsl -Fo ../../../aether3d_build\Samples\shaders\Standard_skin_vert.spv
-dxc -DVULKAN -Ges -spirv -E main -all-resources-bound -T ps_6_0 hlsl\Standard_frag.hlsl -Fo ../../../aether3d_build\Samples\shaders\Standard_frag.spv
-dxc -DVULKAN -DENABLE_SHADOWS -Ges -spirv -E main -all-resources-bound -T ps_6_0 hlsl\Standard_frag.hlsl -Fo ../../../aether3d_build\Samples\shaders\Standard_frag_shadow.spv
-dxc -DVULKAN -DENABLE_SHADOWS_POINT -Ges -spirv -E main -all-resources-bound -T ps_6_0 hlsl\Standard_frag.hlsl -Fo ../../../aether3d_build\Samples\shaders\Standard_frag_shadow_point.spv
-dxc -DVULKAN -Ges -spirv -E CSMain -all-resources-bound -T cs_6_0 hlsl\Bloom.hlsl -Fo ../../../aether3d_build\Samples\shaders\Bloom.spv
-dxc -DVULKAN -Ges -spirv -E CSMain -all-resources-bound -T cs_6_0 hlsl\Blur.hlsl -Fo ../../../aether3d_build\Samples\shaders\Blur.spv
-dxc -DVULKAN -Ges -spirv -E CSMain -all-resources-bound -T cs_6_0 hlsl\ssao.hlsl -Fo ../../../aether3d_build\Samples\shaders\ssao.spv
-dxc -DVULKAN -Ges -spirv -E CSMain -all-resources-bound -T cs_6_0 hlsl\compose.hlsl -Fo ../../../aether3d_build\Samples\shaders\compose.spv
+dxc -DVULKAN -Ges -spirv -E main -all-resources-bound -T vs_6_0 hlsl/sprite_vert.hlsl -Fo ../../../aether3d_build/Samples/shaders/sprite_vert.spv
+dxc -DVULKAN -Ges -spirv -E main -all-resources-bound -T ps_6_0 hlsl/sprite_frag.hlsl -Fo ../../../aether3d_build/Samples/shaders/sprite_frag.spv
+dxc -DVULKAN -Ges -spirv -E main -all-resources-bound -T vs_6_0 hlsl/unlit_cube_vert.hlsl -Fo ../../../aether3d_build/Samples/shaders/unlit_cube_vert.spv
+dxc -DVULKAN -Ges -spirv -E main -all-resources-bound -T ps_6_0 hlsl/unlit_cube_frag.hlsl -Fo ../../../aether3d_build/Samples/shaders/unlit_cube_frag.spv
+dxc -DVULKAN -Ges -spirv -E main -all-resources-bound -T vs_6_0 hlsl/unlit_vert.hlsl -Fo ../../../aether3d_build/Samples/shaders/unlit_vert.spv
+dxc -DVULKAN -Ges -spirv -E main -all-resources-bound -T ps_6_0 hlsl/unlit_frag.hlsl -Fo ../../../aether3d_build/Samples/shaders/unlit_frag.spv
+dxc -DVULKAN -Ges -spirv -E main -all-resources-bound -T vs_6_0 hlsl/unlit_skin_vert.hlsl -Fo ../../../aether3d_build/Samples/shaders/unlit_skin_vert.spv
+dxc -DVULKAN -Ges -spirv -E main -all-resources-bound -T vs_6_0 hlsl/moments_vert.hlsl -Fo ../../../aether3d_build/Samples/shaders/moments_vert.spv
+dxc -DVULKAN -Ges -spirv -E main -all-resources-bound -T vs_6_0 hlsl/moments_skin_vert.hlsl -Fo ../../../aether3d_build/Samples/shaders/moments_skin_vert.spv
+dxc -DVULKAN -Ges -spirv -E main -all-resources-bound -T ps_6_0 hlsl/moments_frag.hlsl -Fo ../../../aether3d_build/Samples/shaders/moments_frag.spv
+dxc -DVULKAN -Ges -spirv -E main -all-resources-bound -T vs_6_0 hlsl/skybox_vert.hlsl -Fo ../../../aether3d_build/Samples/shaders/skybox_vert.spv
+dxc -DVULKAN -Ges -spirv -E main -all-resources-bound -T ps_6_0 hlsl/skybox_frag.hlsl -Fo ../../../aether3d_build/Samples/shaders/skybox_frag.spv
+dxc -DVULKAN -Ges -spirv -E main -all-resources-bound -T vs_6_0 hlsl/depthnormals_vert.hlsl -Fo ../../../aether3d_build/Samples/shaders/depthnormals_vert.spv
+dxc -DVULKAN -Ges -spirv -E main -all-resources-bound -T vs_6_0 hlsl/depthnormals_skin_vert.hlsl -Fo ../../../aether3d_build/Samples/shaders/depthnormals_skin_vert.spv
+dxc -DVULKAN -Ges -spirv -E main -all-resources-bound -T ps_6_0 hlsl/depthnormals_frag.hlsl -Fo ../../../aether3d_build/Samples/shaders/depthnormals_frag.spv
+dxc -DVULKAN -Ges -spirv -E CSMain -all-resources-bound -T cs_6_0 hlsl/LightCuller.hlsl -Fo ../../../aether3d_build/Samples/shaders/LightCuller.spv
+dxc -DVULKAN -Ges -spirv -E main -all-resources-bound -T vs_6_0 hlsl/Standard_vert.hlsl -Fo ../../../aether3d_build/Samples/shaders/Standard_vert.spv
+dxc -DVULKAN -Ges -spirv -E main -all-resources-bound -T vs_6_0 hlsl/Standard_skin_vert.hlsl -Fo ../../../aether3d_build/Samples/shaders/Standard_skin_vert.spv
+dxc -DVULKAN -Ges -spirv -E main -all-resources-bound -T ps_6_0 hlsl/Standard_frag.hlsl -Fo ../../../aether3d_build/Samples/shaders/Standard_frag.spv
+dxc -DVULKAN -DENABLE_SHADOWS -Ges -spirv -E main -all-resources-bound -T ps_6_0 hlsl/Standard_frag.hlsl -Fo ../../../aether3d_build/Samples/shaders/Standard_frag_shadow.spv
+dxc -DVULKAN -DENABLE_SHADOWS_POINT -Ges -spirv -E main -all-resources-bound -T ps_6_0 hlsl/Standard_frag.hlsl -Fo ../../../aether3d_build/Samples/shaders/Standard_frag_shadow_point.spv
+dxc -DVULKAN -Ges -spirv -E CSMain -all-resources-bound -T cs_6_0 hlsl/Bloom.hlsl -Fo ../../../aether3d_build/Samples/shaders/Bloom.spv
+dxc -DVULKAN -Ges -spirv -E CSMain -all-resources-bound -T cs_6_0 hlsl/Blur.hlsl -Fo ../../../aether3d_build/Samples/shaders/Blur.spv
+dxc -DVULKAN -Ges -spirv -E CSMain -all-resources-bound -T cs_6_0 hlsl/ssao.hlsl -Fo ../../../aether3d_build/Samples/shaders/ssao.spv
+dxc -DVULKAN -Ges -spirv -E CSMain -all-resources-bound -T cs_6_0 hlsl/compose.hlsl -Fo ../../../aether3d_build/Samples/shaders/compose.spv
 
