c077c5570b715b1b6467055a8468aa2c464e0583
Timo Wiren <timo.wiren@gmail.com>
2021-05-09 10:09:18 +0300

    Don't write shader in read_mtl

--- Tools/OBJ_Converter/read_mtl.cpp
+++ Tools/OBJ_Converter/read_mtl.cpp
@@ -90,12 +90,12 @@ void ReadMtl( const std::string& path )
             std::string materialName;
             stm >> materialName;
             std::cout << "material " << materialName << std::endl;
-            std::cout << "shaders Standard Standard" << std::endl;
+            //std::cout << "shaders Standard Standard" << std::endl;
             //std::cout << "param_float roughness 0.2" << std::endl;
             //std::cout << "param_vec3 materialSpecular 0.2 0.2 0.2" << std::endl;
 
             ofs << "material " << materialName << std::endl;
-            ofs << "shaders Standard Standard" << std::endl;
+            //ofs << "shaders Standard Standard" << std::endl;
             //ofs << "param_float roughness 0.2" << std::endl;
             //ofs << "param_vec3 materialSpecular 0.2 0.2 0.2" << std::endl;
         }
