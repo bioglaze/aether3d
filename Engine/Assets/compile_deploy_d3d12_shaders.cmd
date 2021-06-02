becdac796f1c51922b6a6eec48f455e972c9f592
Timo Wiren <timo.wiren@gmail.com>
2021-06-02 19:07:45 +0300

    [d3d12] HLSL 6.0

--- Engine/Assets/compile_deploy_d3d12_shaders.cmd
+++ Engine/Assets/compile_deploy_d3d12_shaders.cmd
@@ -1,39 +1,39 @@
 REM SET COMPILER="C:\Program Files (x86)\Windows Kits\10\bin\10.0.18362.0\x64\fxc"
 SET COMPILER=dxc.exe
 
-%COMPILER% /nologo /all_resources_bound /Ges /WX /O3 -Qembed_debug /Zi /T ps_5_1 /Fo ..\..\..\aether3d_build\Samples\shaders\depthnormals_frag.obj hlsl\depthnormals_frag.hlsl
-%COMPILER% /nologo /all_resources_bound /Ges /WX /O3 -Qembed_debug /Zi /T vs_5_1 /Fo ..\..\..\aether3d_build\Samples\shaders\depthnormals_vert.obj hlsl\depthnormals_vert.hlsl
-%COMPILER% /nologo /all_resources_bound /Ges /WX /O3 -Qembed_debug /Zi /T vs_5_1 /Fo ..\..\..\aether3d_build\Samples\shaders\depthnormals_skin_vert.obj hlsl\depthnormals_skin_vert.hlsl
+%COMPILER% /nologo /all_resources_bound /Ges /WX /O3 -Qembed_debug /Zi /T ps_6_0 /Fo ..\..\..\aether3d_build\Samples\shaders\depthnormals_frag.obj hlsl\depthnormals_frag.hlsl
+%COMPILER% /nologo /all_resources_bound /Ges /WX /O3 -Qembed_debug /Zi /T vs_6_0 /Fo ..\..\..\aether3d_build\Samples\shaders\depthnormals_vert.obj hlsl\depthnormals_vert.hlsl
+%COMPILER% /nologo /all_resources_bound /Ges /WX /O3 -Qembed_debug /Zi /T vs_6_0 /Fo ..\..\..\aether3d_build\Samples\shaders\depthnormals_skin_vert.obj hlsl\depthnormals_skin_vert.hlsl
 
-%COMPILER% /nologo /all_resources_bound /Ges /WX /O3 -Qembed_debug /Zi /T ps_5_1 /Fo ..\..\..\aether3d_build\Samples\shaders\moments_frag.obj hlsl\moments_frag.hlsl
-%COMPILER% /nologo /all_resources_bound /Ges /WX /O3 -Qembed_debug /Zi /T vs_5_1 /Fo ..\..\..\aether3d_build\Samples\shaders\moments_vert.obj hlsl\moments_vert.hlsl
-%COMPILER% /nologo /all_resources_bound /Ges /WX /O3 -Qembed_debug /Zi /T vs_5_1 /Fo ..\..\..\aether3d_build\Samples\shaders\moments_skin_vert.obj hlsl\moments_skin_vert.hlsl
+%COMPILER% /nologo /all_resources_bound /Ges /WX /O3 -Qembed_debug /Zi /T ps_6_0 /Fo ..\..\..\aether3d_build\Samples\shaders\moments_frag.obj hlsl\moments_frag.hlsl
+%COMPILER% /nologo /all_resources_bound /Ges /WX /O3 -Qembed_debug /Zi /T vs_6_0 /Fo ..\..\..\aether3d_build\Samples\shaders\moments_vert.obj hlsl\moments_vert.hlsl
+%COMPILER% /nologo /all_resources_bound /Ges /WX /O3 -Qembed_debug /Zi /T vs_6_0 /Fo ..\..\..\aether3d_build\Samples\shaders\moments_skin_vert.obj hlsl\moments_skin_vert.hlsl
 
-%COMPILER% /nologo /all_resources_bound /Ges /WX /O3 -Qembed_debug /Zi /T ps_5_1 /Fo ..\..\..\aether3d_build\Samples\shaders\sdf_frag.obj hlsl\sdf_frag.hlsl
-%COMPILER% /nologo /all_resources_bound /Ges /WX /O3 -Qembed_debug /Zi /T vs_5_1 /Fo ..\..\..\aether3d_build\Samples\shaders\sdf_vert.obj hlsl\sdf_vert.hlsl
+%COMPILER% /nologo /all_resources_bound /Ges /WX /O3 -Qembed_debug /Zi /T ps_6_0 /Fo ..\..\..\aether3d_build\Samples\shaders\sdf_frag.obj hlsl\sdf_frag.hlsl
+%COMPILER% /nologo /all_resources_bound /Ges /WX /O3 -Qembed_debug /Zi /T vs_6_0 /Fo ..\..\..\aether3d_build\Samples\shaders\sdf_vert.obj hlsl\sdf_vert.hlsl
 
-%COMPILER% /nologo /all_resources_bound /Ges /WX /O3 -Qembed_debug /Zi /T ps_5_1 /Fo ..\..\..\aether3d_build\Samples\shaders\skybox_frag.obj hlsl\skybox_frag.hlsl
-%COMPILER% /nologo /all_resources_bound /Ges /WX /O3 -Qembed_debug /Zi /T vs_5_1 /Fo ..\..\..\aether3d_build\Samples\shaders\skybox_vert.obj hlsl\skybox_vert.hlsl
+%COMPILER% /nologo /all_resources_bound /Ges /WX /O3 -Qembed_debug /Zi /T ps_6_0 /Fo ..\..\..\aether3d_build\Samples\shaders\skybox_frag.obj hlsl\skybox_frag.hlsl
+%COMPILER% /nologo /all_resources_bound /Ges /WX /O3 -Qembed_debug /Zi /T vs_6_0 /Fo ..\..\..\aether3d_build\Samples\shaders\skybox_vert.obj hlsl\skybox_vert.hlsl
 
-%COMPILER% /nologo /all_resources_bound /Ges /WX /O3 -Qembed_debug /Zi /T ps_5_1 /Fo ..\..\..\aether3d_build\Samples\shaders\sprite_frag.obj hlsl\sprite_frag.hlsl
-%COMPILER% /nologo /all_resources_bound /Ges /WX /O3 -Qembed_debug /Zi /T vs_5_1 /Fo ..\..\..\aether3d_build\Samples\shaders\sprite_vert.obj hlsl\sprite_vert.hlsl
+%COMPILER% /nologo /all_resources_bound /Ges /WX /O3 -Qembed_debug /Zi /T ps_6_0 /Fo ..\..\..\aether3d_build\Samples\shaders\sprite_frag.obj hlsl\sprite_frag.hlsl
+%COMPILER% /nologo /all_resources_bound /Ges /WX /O3 -Qembed_debug /Zi /T vs_6_0 /Fo ..\..\..\aether3d_build\Samples\shaders\sprite_vert.obj hlsl\sprite_vert.hlsl
 
-%COMPILER% /nologo /all_resources_bound /Ges /WX /O3 -Qembed_debug /Zi /T ps_5_1 /Fo ..\..\..\aether3d_build\Samples\shaders\Standard_frag.obj hlsl\Standard_frag.hlsl
-%COMPILER% /nologo /all_resources_bound /Ges /WX /O3 -Qembed_debug /Zi /T ps_5_1 /DENABLE_SHADOWS /Fo ..\..\..\aether3d_build\Samples\shaders\Standard_frag_shadow.obj hlsl\Standard_frag.hlsl
-%COMPILER% /nologo /all_resources_bound /Ges /WX /O3 -Qembed_debug /Zi /T ps_5_1 /DENABLE_SHADOWS_POINT /Fo ..\..\..\aether3d_build\Samples\shaders\Standard_frag_shadow_point.obj hlsl\Standard_frag.hlsl
-%COMPILER% /nologo /all_resources_bound /Ges /WX /O3 -Qembed_debug /Zi /T vs_5_1 /Fo ..\..\..\aether3d_build\Samples\shaders\Standard_vert.obj hlsl\Standard_vert.hlsl
-%COMPILER% /nologo /all_resources_bound /Ges /WX /O3 -Qembed_debug /Zi /T vs_5_1 /Fo ..\..\..\aether3d_build\Samples\shaders\Standard_skin_vert.obj hlsl\Standard_skin_vert.hlsl
-%COMPILER% /nologo /all_resources_bound /Ges /WX /O3 -Qembed_debug /Zi /T ps_5_1 /Fo ..\..\..\aether3d_build\Samples\shaders\unlit_cube_frag.obj hlsl\unlit_cube_frag.hlsl
-%COMPILER% /nologo /all_resources_bound /Ges /WX /O3 -Qembed_debug /Zi /T vs_5_1 /Fo ..\..\..\aether3d_build\Samples\shaders\unlit_cube_vert.obj hlsl\unlit_cube_vert.hlsl
+%COMPILER% /nologo /all_resources_bound /Ges /WX /O3 -Qembed_debug /Zi /T ps_6_0 /Fo ..\..\..\aether3d_build\Samples\shaders\Standard_frag.obj hlsl\Standard_frag.hlsl
+%COMPILER% /nologo /all_resources_bound /Ges /WX /O3 -Qembed_debug /Zi /T ps_6_0 /DENABLE_SHADOWS /Fo ..\..\..\aether3d_build\Samples\shaders\Standard_frag_shadow.obj hlsl\Standard_frag.hlsl
+%COMPILER% /nologo /all_resources_bound /Ges /WX /O3 -Qembed_debug /Zi /T ps_6_0 /DENABLE_SHADOWS_POINT /Fo ..\..\..\aether3d_build\Samples\shaders\Standard_frag_shadow_point.obj hlsl\Standard_frag.hlsl
+%COMPILER% /nologo /all_resources_bound /Ges /WX /O3 -Qembed_debug /Zi /T vs_6_0 /Fo ..\..\..\aether3d_build\Samples\shaders\Standard_vert.obj hlsl\Standard_vert.hlsl
+%COMPILER% /nologo /all_resources_bound /Ges /WX /O3 -Qembed_debug /Zi /T vs_6_0 /Fo ..\..\..\aether3d_build\Samples\shaders\Standard_skin_vert.obj hlsl\Standard_skin_vert.hlsl
+%COMPILER% /nologo /all_resources_bound /Ges /WX /O3 -Qembed_debug /Zi /T ps_6_0 /Fo ..\..\..\aether3d_build\Samples\shaders\unlit_cube_frag.obj hlsl\unlit_cube_frag.hlsl
+%COMPILER% /nologo /all_resources_bound /Ges /WX /O3 -Qembed_debug /Zi /T vs_6_0 /Fo ..\..\..\aether3d_build\Samples\shaders\unlit_cube_vert.obj hlsl\unlit_cube_vert.hlsl
 
-%COMPILER% /nologo /all_resources_bound /Ges /WX /O3 -Qembed_debug /Zi /T ps_5_1 /Fo ..\..\..\aether3d_build\Samples\shaders\unlit_frag.obj hlsl\unlit_frag.hlsl
-%COMPILER% /nologo /all_resources_bound /Ges /WX /O3 -Qembed_debug /Zi /T vs_5_1 /Fo ..\..\..\aether3d_build\Samples\shaders\unlit_vert.obj hlsl\unlit_vert.hlsl
-%COMPILER% /nologo /all_resources_bound /Ges /WX /O3 -Qembed_debug /Zi /T vs_5_1 /Fo ..\..\..\aether3d_build\Samples\shaders\unlit_skin_vert.obj hlsl\unlit_skin_vert.hlsl
+%COMPILER% /nologo /all_resources_bound /Ges /WX /O3 -Qembed_debug /Zi /T ps_6_0 /Fo ..\..\..\aether3d_build\Samples\shaders\unlit_frag.obj hlsl\unlit_frag.hlsl
+%COMPILER% /nologo /all_resources_bound /Ges /WX /O3 -Qembed_debug /Zi /T vs_6_0 /Fo ..\..\..\aether3d_build\Samples\shaders\unlit_vert.obj hlsl\unlit_vert.hlsl
+%COMPILER% /nologo /all_resources_bound /Ges /WX /O3 -Qembed_debug /Zi /T vs_6_0 /Fo ..\..\..\aether3d_build\Samples\shaders\unlit_skin_vert.obj hlsl\unlit_skin_vert.hlsl
 
-%COMPILER% /nologo /all_resources_bound /Ges /WX /O3 -Qembed_debug /Zi /T cs_5_1 /Fo ..\..\..\aether3d_build\Samples\shaders\LightCuller.obj /E CSMain hlsl\LightCuller.hlsl
-%COMPILER% /nologo /all_resources_bound /Ges /WX /O3 -Qembed_debug /Zi /T cs_5_1 /Fo ..\..\..\aether3d_build\Samples\shaders\Bloom.obj /E CSMain hlsl\Bloom.hlsl
-%COMPILER% /nologo /all_resources_bound /Ges /WX /O3 -Qembed_debug /Zi /T cs_5_1 /Fo ..\..\..\aether3d_build\Samples\shaders\Blur.obj /E CSMain hlsl\Blur.hlsl
-%COMPILER% /nologo /all_resources_bound /Ges /WX /O3 -Qembed_debug /Zi /T cs_5_1 /Fo ..\..\..\aether3d_build\Samples\shaders\ssao.obj /E CSMain hlsl\ssao.hlsl
-%COMPILER% /nologo /all_resources_bound /Ges /WX /O3 -Qembed_debug /Zi /T cs_5_1 /Fo ..\..\..\aether3d_build\Samples\shaders\compose.obj /E CSMain hlsl\compose.hlsl
+%COMPILER% /nologo /all_resources_bound /Ges /WX /O3 -Qembed_debug /Zi /T cs_6_0 /Fo ..\..\..\aether3d_build\Samples\shaders\LightCuller.obj /E CSMain hlsl\LightCuller.hlsl
+%COMPILER% /nologo /all_resources_bound /Ges /WX /O3 -Qembed_debug /Zi /T cs_6_0 /Fo ..\..\..\aether3d_build\Samples\shaders\Bloom.obj /E CSMain hlsl\Bloom.hlsl
+%COMPILER% /nologo /all_resources_bound /Ges /WX /O3 -Qembed_debug /Zi /T cs_6_0 /Fo ..\..\..\aether3d_build\Samples\shaders\Blur.obj /E CSMain hlsl\Blur.hlsl
+%COMPILER% /nologo /all_resources_bound /Ges /WX /O3 -Qembed_debug /Zi /T cs_6_0 /Fo ..\..\..\aether3d_build\Samples\shaders\ssao.obj /E CSMain hlsl\ssao.hlsl
+%COMPILER% /nologo /all_resources_bound /Ges /WX /O3 -Qembed_debug /Zi /T cs_6_0 /Fo ..\..\..\aether3d_build\Samples\shaders\compose.obj /E CSMain hlsl\compose.hlsl
 
 pause
