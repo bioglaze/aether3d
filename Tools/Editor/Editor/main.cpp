eb3aa567942cb7da0d688812f015a6e710f1cbb4
Timo Wiren <timo.wiren@gmail.com>
2021-06-09 20:01:43 +0300

    [editor] Handle Home/End in text input

--- Tools/Editor/Editor/main.cpp
+++ Tools/Editor/Editor/main.cpp
@@ -219,6 +219,8 @@ int main()
                 if (event.keyCode == KeyCode::Enter) inspector.HandleKey( 4, event.type == WindowEventType::KeyDown ? 0 : 1 );
                 if (event.keyCode == KeyCode::Backspace) inspector.HandleKey( 6, event.type == WindowEventType::KeyDown ? 0 : 1 );
                 if (event.keyCode == KeyCode::Delete) inspector.HandleKey( 3, event.type == WindowEventType::KeyDown ? 0 : 1 );
+                if (event.keyCode == KeyCode::Home) inspector.HandleKey( 17, event.type == WindowEventType::KeyDown ? 0 : 1 );
+                if (event.keyCode == KeyCode::End) inspector.HandleKey( 18, event.type == WindowEventType::KeyDown ? 0 : 1 );
                 if (event.type == WindowEventType::KeyDown)
                 {
                     if (event.keyCode == KeyCode::Minus) inspector.HandleChar( '-' );
