bd4bd9c5cdedcc80d2baa01a801e6f2ddbb43f4b
Timo Wiren <timo.wiren@gmail.com>
2021-10-31 07:50:27 +0200

    Bigger particles in HLSL

--- Engine/Assets/hlsl/particle_draw.hlsl
+++ Engine/Assets/hlsl/particle_draw.hlsl
@@ -7,9 +7,14 @@ void CSMain( uint3 globalIdx : SV_DispatchThreadID, uint3 localIdx : SV_GroupThr
     
     for (uint i = 0; i < particleCount; ++i)
     {
-        if ((uint)particles[ i ].clipPosition.x + windowWidth / 2 == globalIdx.x && (uint)particles[ i ].clipPosition.y + windowHeight / 2 == globalIdx.y)
+        //if ((uint)particles[ i ].clipPosition.x + windowWidth / 2 == globalIdx.x && (uint)particles[ i ].clipPosition.y + windowHeight / 2 == globalIdx.y)
         {
-            color += particles[ i ].color;
+            float dist = distance( particles[ i ].clipPosition.xy + float2( windowWidth, windowHeight ) / 2, globalIdx.xy );
+            const float radius = 5;
+            if (dist < radius)
+            {
+                color = particles[ i ].color;
+            }
         }
     }
 
