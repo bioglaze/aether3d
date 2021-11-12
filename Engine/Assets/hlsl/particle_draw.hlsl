5b2cb076bdc89a2b75dce34b7c133870cc624ad1
Timo Wiren <timo.wiren@gmail.com>
2021-11-12 14:56:05 +0200

    [d3d12] Fix particle transformation

--- Engine/Assets/hlsl/particle_draw.hlsl
+++ Engine/Assets/hlsl/particle_draw.hlsl
@@ -7,7 +7,10 @@ void CSMain( uint3 globalIdx : SV_DispatchThreadID, uint3 localIdx : SV_GroupThr
     
     for (uint i = 0; i < particleCount; ++i)
     {
-        float4 clipPos = mul( viewToClip, particles[ i ].position );        
+        float4 clipPos = mul( viewToClip, particles[ i ].position );    
+#if !VULKAN
+        clipPos.y = -clipPos.y;
+#endif
         float3 ndc = clipPos.xyz / clipPos.w;
         float3 unscaledWindowCoords = 0.5f * ndc + float3( 0.5f, 0.5f, 0.5f );
         float3 windowCoords = float3( windowWidth * unscaledWindowCoords.x, windowHeight * unscaledWindowCoords.y, unscaledWindowCoords.z );
