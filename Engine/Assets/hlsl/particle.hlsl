af61a99468260b0f97ad8ca806bb39a96de231a7
Timo Wiren <timo.wiren@gmail.com>
2021-11-21 08:56:51 +0200

    Optimize particles

--- Engine/Assets/hlsl/particle.hlsl
+++ Engine/Assets/hlsl/particle.hlsl
@@ -12,10 +12,18 @@ void CSMain( uint3 globalIdx : SV_DispatchThreadID, uint3 localIdx : SV_GroupThr
     float2 uv = globalIdx.xy;// / float2( windowWidth, windowHeight );
     float ra = rand_1_05( uv );
     float x = 1;//rand_1_05( uv );
-    //float4 position = float4( globalIdx.x * x * 8, sin( timeStamp * 10 ) * 16, 0, 1 );
-    //float4 position = float4( globalIdx.x * x * 8 * sin( timeStamp * 10 ), 0, globalIdx.y * x * 8 * cos( timeStamp * 10 ), 1 );
-    float4 position = float4(  x * 8 * sin( globalIdx.x * 10 + timeStamp ), globalIdx.x % 10,  x * 8 * cos( globalIdx.x * 10 + timeStamp ), 1 );
+    float4 position = float4(  x * 8 * sin( globalIdx.x * 20 + timeStamp ), globalIdx.x % 20, x * 8 * cos( globalIdx.x * 20 + timeStamp ), 1 );
     particles[ globalIdx.x ].position = position;
-    particles[ globalIdx.x ].color = float4( 1, 0, 0, 1 );
-    particles[ globalIdx.x ].clipPosition = position;//mul( viewToClip, position );
+    particles[ globalIdx.x ].color = float4( 1, 1, 1, 1 );
+
+    float4 clipPos = mul( viewToClip, position );
+    
+#if !VULKAN
+    clipPos.y = -clipPos.y;
+#endif
+    float3 ndc = clipPos.xyz / clipPos.w;
+    float3 unscaledWindowCoords = 0.5f * ndc + float3( 0.5f, 0.5f, 0.5f );
+    float3 windowCoords = float3( windowWidth * unscaledWindowCoords.x, windowHeight * unscaledWindowCoords.y, unscaledWindowCoords.z );
+
+    particles[ globalIdx.x ].clipPosition = float4( windowCoords.x, windowCoords.y, 5, 1 );
 }
--- Engine/Assets/hlsl/particle_draw.hlsl
+++ Engine/Assets/hlsl/particle_draw.hlsl
@@ -7,16 +7,9 @@ void CSMain( uint3 globalIdx : SV_DispatchThreadID, uint3 localIdx : SV_GroupThr
     
     for (uint i = 0; i < particleCount; ++i)
     {
-        float4 clipPos = mul( viewToClip, particles[ i ].position );    
-#if !VULKAN
-        clipPos.y = -clipPos.y;
-#endif
-        float3 ndc = clipPos.xyz / clipPos.w;
-        float3 unscaledWindowCoords = 0.5f * ndc + float3( 0.5f, 0.5f, 0.5f );
-        float3 windowCoords = float3( windowWidth * unscaledWindowCoords.x, windowHeight * unscaledWindowCoords.y, unscaledWindowCoords.z );
-
-        float dist = distance( windowCoords.xy, globalIdx.xy );
-        const float radius = 5;
+        float dist = distance( particles[ i ].clipPosition.xy, globalIdx.xy );
+        const float radius = particles[ i ].clipPosition.z;
+        
         if (dist < radius)
         {
             color = particles[ i ].color;
